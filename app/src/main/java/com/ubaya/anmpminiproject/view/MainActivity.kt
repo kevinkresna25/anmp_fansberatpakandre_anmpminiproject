c9c404059c46e7bb3c9d1577e728be99a3f07c21
kevinkresna25 <kevinkresna25@gmail.com>
2025-10-05 22:43:16 +0700

    initial commit

---
+++ app/src/main/java/com/ubaya/anmpminiproject/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ubaya.anmpminiproject.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ubaya.anmpminiproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
