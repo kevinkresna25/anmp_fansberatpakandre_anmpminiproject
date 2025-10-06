51dd70af00bd161a113a153606db6d59eb0322e9
kevinkresna25 <kevinkresna25@gmail.com>
2025-10-06 22:20:02 +0700

    create ProfilViewModel

---
+++ app/src/main/java/com/ubaya/anmpminiproject/viewmodel/ProfilViewModel.kt
@@ -0,0 +1,31 @@
+package com.ubaya.anmpminiproject.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.ubaya.anmpminiproject.util.SharedPrefHelper
+
+class ProfilViewModel(application: Application) : AndroidViewModel(application) {
+    private val sharedPrefHelper = SharedPrefHelper(application)
+
+    val nama = MutableLiveData<String>()
+    val tglLahir = MutableLiveData<String>()
+    val jenisKelamin = MutableLiveData<String>()
+    val saveStatus = MutableLiveData<Boolean>()
+
+    fun loadProfil() {
+        nama.value = sharedPrefHelper.getNama()
+        tglLahir.value = sharedPrefHelper.getTglLahir()
+        jenisKelamin.value = sharedPrefHelper.getJenisKelamin()
+    }
+
+    fun simpanProfil(nama: String, tglLahir: String, jenisKelamin: String) {
+        try {
+            sharedPrefHelper.simpanProfil(nama, tglLahir, jenisKelamin)
+            saveStatus.value = true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            saveStatus.value = false
+        }
+    }
+}
