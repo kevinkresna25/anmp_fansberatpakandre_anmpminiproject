f6b94642899765385817070465ac1b9bb18d0d7e
kevinkresna25 <kevinkresna25@gmail.com>
2025-10-06 13:35:47 +0700

    binding recView in DataAdapter

--- app/src/main/java/com/ubaya/anmpminiproject/view/DataAdapter.kt
+++ app/src/main/java/com/ubaya/anmpminiproject/view/DataAdapter.kt
@@ -1,4 +1,36 @@
 package com.ubaya.anmpminiproject.view
 
-class DataAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ubaya.anmpminiproject.databinding.PengukuranListItemBinding
+import com.ubaya.anmpminiproject.model.Pengukuran
+
+class DataAdapter(private var pengukuranList: ArrayList<Pengukuran>)
+    : RecyclerView.Adapter<DataAdapter.PengukuranViewHolder>()  {
+
+    class PengukuranViewHolder(var binding: PengukuranListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PengukuranViewHolder {
+        val binding = PengukuranListItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return PengukuranViewHolder(binding)
+    }
+
+    override fun getItemCount() = pengukuranList.size
+
+    override fun onBindViewHolder(holder: PengukuranViewHolder, position: Int) {
+        val pengukuran = pengukuranList[position]
+        holder.binding.txtUsiaValue.text = "${pengukuran.usia} tahun"
+        holder.binding.txtTinggiValue.text = "${pengukuran.tinggi} cm"
+        holder.binding.txtBeratValue.text = "${pengukuran.berat} kg"
+    }
+
+    fun updateData(newPengukuranList: List<Pengukuran>) {
+        pengukuranList.clear()
+        pengukuranList.addAll(newPengukuranList)
+        notifyDataSetChanged()
+    }
 }
